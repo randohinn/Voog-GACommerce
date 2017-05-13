5cda3e00dd413e4122594dff40fc8eb31e666d08
Rando Hinn <randohinn@gmail.com>
2017-05-13 16:51:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Voog-GACommerce
+[![Voog badge](https://img.shields.io/badge/Voog-voog.com-blue.svg)](https://voog.com)
+
+Voog-GACommerce is a simple drop-in solution for tracking [Voog](https://voog.com) E-Commerce purchases in the [Google Analytics](https://analytics.google.com) Ecommerce section.
+
+## Usage
+
+You need to have GA set up on your site beforehand.
+
+* Download the script and upload it to your components folder in the editor. In `Settings` -> `Site`, add this inside the already present GA script tag under External stats, after `ga('send', 'pageview');`
+
+```
+    var s = document.createElement( 'script' );
+    s.setAttribute( 'src', '/javascripts/Voog-GACommerce.js' );
+    document.body.appendChild( s );
+``` 
+
+This makes sure, that GACommerce loads after GA has been set up and exists to be used.
+
+* Start buying, the script will now automatically detect checkouts and send data to GA.  Processing might take some time.
+
+**Since Voog doesn't have categories in the Store feature itself, all items will be sent with category 'Product'.**
+
+## Contributing
+
+Feel free to Fork and PR this repo, aswell as submit any issues.
---
+++ Voog-GACommerce.js
@@ -0,0 +1,32 @@
+ga('require', 'ecommerce'); // We need Google Analytics to actually know what we want to do. Thus, give us Ecommerce!
+
+(function () {
+    $(document).on('voog:shoppingcart:show', (event) => {
+        if(event.detail.view === "review") {
+            let shoppingcart = Voog.ShoppingCart.getShoppingCartData();
+            ga('ecommerce:clear'); // Existing unsent data from before? Make it dissapear
+            ga('ecommerce:addTransaction', {
+                'id': shoppingcart.uuid,                                      // Transaction ID.
+                'affiliation': VoogEcommerce.storeInfo.company_name,          // Affiliation or store name.
+                'revenue': shoppingcart.total_amount.toString(),              // Grand Total.
+                'shipping': shoppingcart.shipping_subtotal_amount.toString(), // Shipping.
+                'tax': shoppingcart.items_tax_amount.toString()               // Tax.
+            });
+            shoppingcart.items.forEach(function(item) {
+                ga('ecommerce:addItem', {
+                    'id': shoppingcart.uuid,
+                    'name': item.name,
+                    'sku': item.product.name, // Yes, this is not a SKU, but the review view doesn't seem to have 'em...
+                    'category': 'Product',
+                    'price': item.product.price.toString(),
+                    'quantity': item.quantity.toString(),
+                    'currency': VoogEcommerce.storeInfo.currency
+                });
+            }, this);
+        }
+    });
+
+    $(document).on('voog:shoppingcart:choosepaymentmethod', () => {
+        ga('ecommerce:send');
+    });
+})();
